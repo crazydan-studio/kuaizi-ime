56bdf44b86a4a4c4c4aad4bf7bf082a513781a5c
flytreeleft <flytreeleft@crazydan.org>
2026-04-01 10:37:05 +0800

    tools/pinyin-dict: 调整字典数据库文件名

--- tools/pinyin-dict/src/db/utils.mjs
+++ tools/pinyin-dict/src/db/utils.mjs
@@ -2,9 +2,9 @@ import { fromRootPath } from '#utils/utils.mjs';
 
 /** 获取 SQLite 字典库文件路径 */
 export function getZiDictSQLiteFile() {
-  return fromRootPath('data', 'pinyin-zi-dict.sqlite');
+  return fromRootPath('data', 'pinyin-dict.sqlite');
 }
 
 export function getZiDictSQLiteVersionFile(version) {
-  return fromRootPath('data', 'pinyin-zi-dict.' + version + '.sqlite');
+  return fromRootPath('data', 'pinyin-dict.' + version + '.sqlite');
 }
